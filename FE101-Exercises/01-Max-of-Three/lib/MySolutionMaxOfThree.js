c01fafeb05ab5a4755a660c3eedb75077ef390c4
mahitap tokpunar <mahitaptokpinar@gmail.com>
2016-07-20 08:04:38 -0400

    My solution to Max of three problem by mahitap

---
+++ FE101-Exercises/01-Max-of-Three/lib/MySolutionMaxOfThree.js
@@ -0,0 +1,41 @@
+var maxOfThree=(a,b,c)=>{
+  var max=a;
+
+  if(b>max){
+    max=b;
+  }
+
+  if(c>max){
+    max=c;
+  }
+  return max;
+}
+// TODO: Implement the maxOfThree function
+
+module.exports = maxOfThree; // DO NOT CHANGE THIS
+
+
+//1'den fazla sayıdan en büyüğünü bulma.
+//1. yol
+function maxOfThree(arr) {
+
+  var max = arr[0];
+
+  arr.forEach(function (elm) {
+    max = elm > max ? elm : max;
+  })
+
+  return max;
+}
+
+//2.yol
+
+function maxOfThree2(arr) {
+  var max = arr[0];
+
+  for(var i=0;i<arr.length;i++) {
+    max = arr[i] > max ? arr[i] : max;
+  }
+
+  return max;
+}
