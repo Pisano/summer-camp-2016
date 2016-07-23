c6c295f16ab3e2c197ef4f067f69cbdbc1a0812a
Hanefi Onaldi <abdullahanefi16@gmail.com>
2016-07-23 17:53:37 +0300

    implemented the first 2 exercises of BE101

--- Hanefi/BE101-Exercises/02-Minimum-Path-Sum/lib/triangle.rb
+++ Hanefi/BE101-Exercises/02-Minimum-Path-Sum/lib/triangle.rb
@@ -11,5 +11,24 @@ class Triangle
 
   def minimum_path_sum
     # TODO: return an integer = the minimum path sum of the triangle (in /data)
+    prev = [0]
+    @rows.each do |row|
+      row = row.split(" ")
+      row.each_with_index do |val, index|
+        row[index] = val.to_i
+        if index > 0
+          row[index] += min(prev[index], prev[index - 1])
+        else
+          row[index] += prev[index]
+        end
+      end
+      prev = row
+    end
+
+    prev.min
+  end
+
+  def min(*values)
+    values.select { |n| n }.min
   end
 end
